060e834b2d31ee5881a91d66fff4799794fbf50e
osheyboi <osheyboi@outlook.com.au>
2026-05-22 11:52:17 +1000

    Update Readme

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ pygame.display.set_caption('Proto Clicker 2')
 #Base Veriables
 clicks = 0
 rebirths = 0
+Menu = 0
 
 shop_menu = pygame.Rect(460, 720, 440, 140)
 Rebirth_menu = pygame.Rect(24, 227, 100, 100)
@@ -33,12 +34,14 @@ background = pygame.Rect(0, 0, 1300, 900)
 Clicks_Amount_Box = pygame.Rect(250, 20, 350, 100)
 Rebirth_Amount_Box = pygame.Rect(800, 20, 350, 100)
 RebirthIcon = pygame.Rect(800, 20, 350, 100)
-Button_center = (675, 400)  # Centered exactly where your 100x100 box was
+Button_center = (675, 400)
 Button_radius = 200
 
 RebirthIcon =  pygame.image.load("Rebirth.png")
 Rebirthicon_Resize = pygame.transform.scale(RebirthIcon, (70, 60))
 text1 = font.render("Shop", True, (0, 0, 0))
+
+Menu_Box = pygame.Rect(50, 150, 1200, 700)
 
 
 running = True
@@ -60,15 +63,17 @@ while running:
                     print("Rebirth")
                 if shop_menu.collidepoint(mouse_pos):
                     print("Shop")
+                    Menu = 1
 
                 if distance <= Button_radius:
                     clicks += 1
                     print(clicks)
 
+    #Menu System (Functions)
 
 
 
-    #Menu System
+
 
     # Drawing Systems
     pygame.draw.rect(screen, gray, background)
@@ -96,10 +101,17 @@ while running:
     CurrencyBox2.center = (950, 75)
     Text1.center = (651, 794)
 
+
     screen.blit(Rebirthicon_Resize, (40, 247))
     screen.blit(Clicks_AR, CurrencyBox1)
     screen.blit(Rebirth_AR, CurrencyBox2)
     screen.blit(text1, Text1)
+
+    # Menu System (Drawing)
+    if Menu != 0:
+        pygame.draw.rect(screen, green, Menu_Box, width=0, border_radius=50)
+        pygame.draw.rect(screen, black, Menu_Box, width=5, border_radius=50)
+
     pygame.display.flip()
     clock.tick(60)
 
