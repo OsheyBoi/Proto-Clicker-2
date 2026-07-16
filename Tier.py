cccf27688da160da7ecac6c78eda734c6309bb7b
osheyboi <osheyboi@outlook.com.au>
2026-07-16 10:02:55 +1000

    Fix some text spelling issues Add Code to show that text and cost for tiers up to 10

--- Tier.py
+++ Tier.py
@@ -36,9 +36,24 @@ def tier_info (Tier):
          text = " \n Tier 4: \n - Unlock Click Upgrade 5 \n - 3x Clicks \n  - 2x Rebirths \n - 1.25x Faster Button Cooldown"
          return text
     elif Tier == 4:
-         text = " \n Tier 5: \n - Unlock Rebirth Upgrade 4 \n - Auto Rebirth (1% RPS) \n - 2x Clicks/Rebirths \n - Powerfull Clicks Power +0.05 and Max +10  \n Playtime Boost Clicks"
+         text = " \n Tier 5: \n - Unlock Rebirth Upgrade 4 \n - Auto Rebirth (1% RPS) \n - 2x Clicks/Rebirths \n - Powerful Clicks Power +0.05 and Max +10  \n Playtime Boost Clicks"
          return text
     elif Tier == 5:
+         text = " \n Tier 6: \n Rebirths Boost Clicks \n Rebirths Boost themself \n Clicks Power 2 +15 Max / +0.05x \n (+ Price Increase) "
+         return text
+    elif Tier == 6:
+         text = " \n  Tier 7: \n Powerful Clicks +0.2x / + 5 Max \n (+ Price Increase) \n x1.25 Faster Button Cooldown"
+         return text
+    elif Tier == 7:
+         text = " \n  Tier 8: \n 4x Xp \n 1.01^ Clicks \n 25% Faster Autoclicker. "
+         return text
+    elif Tier == 8:
+         text = " \n  Tier 9: Base Power +1 -> +2 / 4x Cap \n (+ Price Increase) \n 1.01^ - 1.02^ Clicks"
+         return text
+    elif Tier == 9:
+         text = " \n  Tier 10: Powerful Clicks +0.5x Mult \n 1.02^ - 1.03^ Clicks \n Unlock Ascension (V3)"
+         return text
+    elif Tier == 10:
          text = " \n You Have Reach Max Tier in V1.0 \n More Will Be add In V2.0 "
          return text
 
@@ -50,22 +65,46 @@ def tier_info (Tier):
 
 def tier_cost(tier, type):
     if tier == 0:
+        # 1 Thousand
         Cost = 1000
 
     elif tier == 1:
+        #50 Thousand
         Cost = 50000
 
     elif tier == 2:
+        #1 Million
         Cost = 1000 ** 2
 
     elif tier == 3:
+        #25 Million
         Cost = 25000000
 
     elif tier == 4:
+        # 500 Million
         Cost = 500000000
 
-
     elif tier == 5:
+        # 5 Billion
+        Cost = (1000 ** 3) * 5
+
+    elif tier == 6:
+        # 500  Billion
+        Cost = (1000 ** 3) * 500
+
+    elif tier == 7:
+        # 100 trillion
+        Cost = (1000 ** 4) * 100
+
+    elif tier == 8:
+        # 750 quadrillion
+        Cost = (1000 ** 5) * 750
+
+    elif tier == 9:
+        # 1 sextillion
+        Cost = (1000 ** 5) * 750
+
+    elif tier == 10:
         Cost =  10000**10
         type = "Max Tier"
 
